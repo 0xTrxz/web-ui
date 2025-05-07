6dbe29541fa3e14bebd3ff637b4e66147e54abdc
ModernSocietyLabs <hello@modernsociety.xyz>
2025-05-06 19:00:42 -0500

    Fix application startup and health check issues for Railway deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Start supervisord in the foreground to properly manage child processes
-exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
+# Start supervisord directly
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
